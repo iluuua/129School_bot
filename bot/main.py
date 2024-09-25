4a53a4e50a61a3d3ee2ac7661528bf45573a9f43
ilua <ilyamihailovsy@gmail.com>
2024-09-26 00:25:57 +0300

    Initial commit

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from telegram_bot import bot
 from web_app.app import app
 
@@ -6,6 +7,11 @@ def main():
     bot.polling(none_stop=True)
 
 
-if __name__ == '__main__':
+def running():
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    # t = Thread(target=running())
+    # t.start()
     main()
